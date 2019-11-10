dea15be38c16d037fe6b5a4e2d3a1c6b163e0282
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-11-10 04:18:29 +0200

    Updates

---
+++ precache-manifest.fd5709ec75b12738cd89e6baa71c5410.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb5407eaeee5c61426a65085315e843",
+    "url": "/scraper-app/index.html"
+  },
+  {
+    "revision": "19e0b1f7ed0d9c45e79d",
+    "url": "/scraper-app/static/css/main.00b75743.chunk.css"
+  },
+  {
+    "revision": "83179499ae39aa89161e",
+    "url": "/scraper-app/static/js/2.1e2c5f9d.chunk.js"
+  },
+  {
+    "revision": "19e0b1f7ed0d9c45e79d",
+    "url": "/scraper-app/static/js/main.cc00e60f.chunk.js"
+  },
+  {
+    "revision": "31dda78a81f3225d3d27",
+    "url": "/scraper-app/static/js/runtime-main.628167aa.js"
+  }
+]);
